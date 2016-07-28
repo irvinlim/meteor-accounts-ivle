bffb6849f39d10c153f2b4a4e49bf38c0c5b2a14
Irvin Lim <irvinlimweiquan@gmail.com>
2016-07-28 16:26:01 +0800

    Update package.js

--- package.js
+++ package.js
@@ -1,19 +1,17 @@
 Package.describe({
   name: 'irvinlim:accounts-ivle',
-  version: '0.0.4',
+  version: '0.0.5',
   summary: 'Login service for NUS IVLE accounts.',
   git: 'https://github.com/irvinlim/meteor-accounts-ivle',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.2.4');
-  api.use('ecmascript');
   api.use('underscore');
   api.use('accounts-base', ['client', 'server']);
   api.imply('accounts-base', ['client', 'server']);
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('irvinlim:ivle@0.0.4', ['client', 'server']);
+  api.use('irvinlim:ivle', ['client', 'server']);
 
   api.addFiles('client/login.js', 'client');
   api.addFiles('lib/register.js');
